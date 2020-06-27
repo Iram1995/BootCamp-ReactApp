78020cda9891867dc3c4a10182bb18ceef20a881
Iram1995 <52947627+Iram1995@users.noreply.github.com>
2020-06-27 18:56:49 +0500

    App.js File updated

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Good to learn 
-        </a>
-      </header>
+      <h1>Hello World from Iram Manzoor</h1>
     </div>
   );
 }
